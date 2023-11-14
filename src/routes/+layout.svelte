61e9f08178396ac51a19b39197a3a26b41e6dcc7
Mike Peiman <mikepeiman@gmail.com>
2023-11-14 12:47:17 -0500

    installed Flowbite-svelte UI library

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.postcss';
 	import '../app.scss';
+	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+</AppShell>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
+<script>
+	import FlowbiteExample1 from '../components/FlowbiteExample1.svelte';
+</script>
+
 <div class="w-full">
-    <div class="p-24">
-        <div class="bg-blue-900 p-24 rounded-[2rem]">
-            <div class="flex flex-col flex-1 justify-center items-center">
-                <h1 class="text-cyan-400 text-4xl">Welcome to SvelteKit</h1>
-                <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-            </div>
-        </div>
-    </div>
+	<div class="p-24">
+		<div class="bg-blue-900 p-24 rounded-[2rem]">
+			<div class="flex flex-col flex-1 justify-center items-center">
+				<h1 class="text-cyan-400 text-4xl">Welcome to SvelteKit</h1>
+				<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+				<FlowbiteExample1 />
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
-    /* your styles go here */
+	/* your styles go here */
 </style>
