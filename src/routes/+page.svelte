5e66cb4b234ef753841fc7858ff32a2c1d7049b7
Mike Peiman <mikepeiman@gmail.com>
2023-11-14 09:47:59 -0500

    added multiple packages - tailwindcss, scss, skeletonUI. applied basic styling to confirm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full">
+    <div class="p-24">
+        <div class="bg-blue-900 p-24 rounded-[2rem]">
+            <div class="flex flex-col flex-1 justify-center items-center">
+                <h1 class="text-cyan-400 text-4xl">Welcome to SvelteKit</h1>
+                <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+    /* your styles go here */
+</style>
